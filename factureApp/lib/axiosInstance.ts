87b923d9fb320fa297dc7a7b603ec151f950a7c0
Marcssarutobi <soheloris@gmail.com>
2026-04-19 10:17:26 +0200

    fix: Set withCredentials to false in axios instance configuration

--- factureApp/lib/axiosInstance.ts
+++ factureApp/lib/axiosInstance.ts
@@ -6,6 +6,7 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
     "Accept":       "application/json",
   },
+  withCredentials: false,
 })
 
 // Intercepteur requête : injecte le token à chaque appel
