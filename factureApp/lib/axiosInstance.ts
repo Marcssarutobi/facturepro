6ab3beeb8d4955ceeaab14da7d0c8445122b10af
hstkbj <hstkbj@asiso.io>
2026-04-28 16:24:34 +0100

    feat: Uncomment baseURL configuration in axios instance

--- factureApp/lib/axiosInstance.ts
+++ factureApp/lib/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
+  //baseURL: process.env.NEXT_PUBLIC_API_URL,
   baseURL: '/proxy',
   headers: {
     "Content-Type": "application/json",
