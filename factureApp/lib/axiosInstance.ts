1f153af60606d68033cf445670774ca5906b3992
hstkbj <hstkbj@asiso.io>
2026-04-28 16:23:29 +0100

    feat: Configure axios instance with proxy and add Vercel rewrites for API routing

--- factureApp/lib/axiosInstance.ts
+++ factureApp/lib/axiosInstance.ts
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: '/proxy',
   headers: {
     "Content-Type": "application/json",
     "Accept":       "application/json",
   },
-  withCredentials: true,
 })
 
 // Intercepteur requête : injecte le token à chaque appel
